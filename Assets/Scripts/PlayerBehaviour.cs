4e28a6967b06116441209573056f8b7bba49f937
Lea <lea.brechet@yahoo.fr>
2020-10-19 11:43:48 +0200

    No anim but Jump

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -8,6 +8,7 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private float Speed;
     [SerializeField] private float MaxSpeed;
+    [SerializeField] private float JumpForce;
 
     private Inputs inputs;
     private Vector2 direction;
@@ -18,6 +19,8 @@ public class PlayerBehaviour : MonoBehaviour
     private Rigidbody2D myRigidbody2D;
     private SpriteRenderer mySpriteRenderer;
 
+    private bool isMoving = false;
+
 
     private void OnEnable()
     {
@@ -25,6 +28,8 @@ public class PlayerBehaviour : MonoBehaviour
         inputs.Enable();
         inputs.Player.Move.performed += OnMovePerformed;
         inputs.Player.Move.canceled += OnMoveCanceled;
+
+        inputs.Player.Jump.performed += OnJumpPerformed;
 
         myAnimator = GetComponent<Animator>();
         myRigidbody2D = GetComponent<Rigidbody2D>();
@@ -47,13 +52,14 @@ public class PlayerBehaviour : MonoBehaviour
     private void FixedUpdate()
     {
         var PlayerDirection = new Vector2(direction.x, 0);
+
         if(myRigidbody2D.velocity.sqrMagnitude < MaxSpeed)
         {
            myRigidbody2D.AddForce(direction * Speed);
         }
 
         /*var isRunning = PlayerDirection.x != 0;
-        myAnimator.SetBool("IsRunning", isRunning);*/ 
+        myAnimator.SetBool("IsRunning", isRunning);*/
         //NEFONCTIONNEPAS
         //ANIMATION DE COURSE NE SE LANCE PAS
 
@@ -65,6 +71,8 @@ public class PlayerBehaviour : MonoBehaviour
         {
             mySpriteRenderer.flipX = true;
         }
+
+        
 
         /*var IsAscending = !IsOnGround && myRigidbody2D.velocity.y > 0;
         myAnimator.SetBool("IsJumping", IsAscending);
@@ -84,5 +92,26 @@ public class PlayerBehaviour : MonoBehaviour
     private void OnMoveCanceled(InputAction.CallbackContext obj)
     {
         direction = Vector2.zero;
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext obj)
+    {
+        if (IsOnGround == true)
+        {
+            myRigidbody2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            IsOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        /*// Booléen vérifiant si le layer sur lequel on a atteri appartient bien au layerMask "ground"
+        var touchGround = ground == (ground | (1 << other.gameObject.layer));
+        // Booléen vérifiant que l'on collisionne avec une surface horizontale*/
+        var touchFromAbove = other.contacts[0].normal == Vector2.up;
+        if (other.gameObject.CompareTag("Ground") == true) //&& touchFromAbove == true)
+        {
+            IsOnGround = true;
+        }
     }
 }
