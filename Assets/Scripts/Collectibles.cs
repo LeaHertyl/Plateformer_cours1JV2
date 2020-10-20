195d58cb4da1582c70d38928f49f595cc412a1f6
Lea <lea.brechet@yahoo.fr>
2020-10-20 09:54:16 +0200

    New script + modif prefab player + collectible script ok
    Ajouter les UI

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Collectibles : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI Score;
+    private int ScoreValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreValue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score.text = "Score : " + ScoreValue;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("FlyCollectible"))
+        {
+            Debug.Log("youpi");
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
