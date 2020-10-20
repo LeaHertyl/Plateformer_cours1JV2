305e8392b6a828e40040cd834d535f7b7b14f977
Lea <lea.brechet@yahoo.fr>
2020-10-20 09:39:46 +0200

    collectibles prefab + add UI using in script

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerBehaviour : MonoBehaviour
 {
