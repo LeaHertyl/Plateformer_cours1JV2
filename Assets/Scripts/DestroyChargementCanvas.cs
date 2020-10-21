0a868f81300498d1944b80ed29b855608873b95d
Lea <lea.brechet@yahoo.fr>
2020-10-21 11:00:14 +0200

    Add destroy canvas script

---
+++ Assets/Scripts/DestroyChargementCanvas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyChargementCanvas : MonoBehaviour
+{
+
+    //on cree une fonction destroy publique pour y acceder dans l'inspector pour detruire le gameobject lorsque qu'elle est activée
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
